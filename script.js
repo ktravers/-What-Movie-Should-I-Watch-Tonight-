94acdaafe9cb8b63eea445106aa4ec4171133f3b
Kate Travers <8152930+ktravers@users.noreply.github.com>
2021-04-12 21:24:31 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".pick-movie").click(function () {
+    let name = $(".name").val();
+    console.log("name: " + name);
+    
+    let grade = $(".grade").val();
+    console.log("grade: " + grade);
+    
+    let favoriteActor = $(".favorite-actor").val();
+    console.log("favorite actor: " + favoriteActor);
+   
+    $(".questions").hide();
+    
+    $(".answer").append("<h2>Hey " + name + ", you should watch 👩‍🎤 <a href='https://www.imdb.com/title/tt0236348/'>Josie and the Pussycats</a> 🎸tonight!</h2><h3>It won 🏆 " + grade + " Oscars 🏆, and it's " + favoriteActor + "'s favorite movie! 🤩</h3></h2>");
+   
+});
