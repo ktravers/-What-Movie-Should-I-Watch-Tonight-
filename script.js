560b82ceb677069e29f9cf60a7a1eee4482c37e3
Kate Travers <8152930+ktravers@users.noreply.github.com>
2021-04-13 00:20:43 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-$(".pick-movie").click(function () {
+// Click handler for the "Tell me!" button
+// On click, hides the inputs and displays a personalized movie recommendation
+$(".pick-movie").click(function() {
+    // Store the values the user entered in local variables
     let name = $(".name").val();
-    console.log("name: " + name);
-    
     let grade = $(".grade").val();
-    console.log("grade: " + grade);
-    
     let favoriteActor = $(".favorite-actor").val();
+
+    // Log the user inputs to console for easier debugging
+    console.log("name: " + name);
+    console.log("grade: " + grade);
     console.log("favorite actor: " + favoriteActor);
-   
+
+    // Hide the questions, inputs, and "Tell me!" button
     $(".questions").hide();
-    
-    $(".answer").append("<h2>Hey " + name + ", you should watch 👩‍🎤 <a href='https://www.imdb.com/title/tt0236348/'>Josie and the Pussycats</a> 🎸tonight!</h2><h3>It won 🏆 " + grade + " Oscars 🏆, and it's " + favoriteActor + "'s favorite movie! 🤩</h3></h2>");
-    
+
+    // Build a personalized movie recommendation message from the values the user entered
+    let movieRecommendation = "<h2>Hey " + name + ", you should watch 👩‍🎤 <a href='https://www.imdb.com/title/tt0236348/'>Josie and the Pussycats</a> 🎸tonight!</h2><h3>It won 🏆 " + grade + " Oscars 🏆, and it's " + favoriteActor + "'s favorite movie! 🤩</h3></h2>";
+
+    // Add the movie recommendation to the page
+    $(".answer").append(movieRecommendation);
+
+    // Show the "Try again" button
     $(".retry").show();
-   
 });
 
+// Click handler for the "Try again" button
+// On click, reloads the page
 $(".retry").click(function() {
+    // Fancy JavaScript that reloads the page
+    // https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
     location.reload();
 });
